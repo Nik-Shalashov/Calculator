0ca463fce434005eb726514c03a8a06cae15a896
Nik-Shalashov <monitor1@inbox.ru>
2021-02-10 21:21:55 +0700

    Реализовал обработку кнопок

--- app/src/main/java/ru/android1/calculator/MainActivity.java
+++ app/src/main/java/ru/android1/calculator/MainActivity.java
@@ -3,6 +3,12 @@ package ru.android1.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
